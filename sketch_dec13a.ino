f925e6c4572a095574751748f87673dd1bdc6251
Pedro Henrique Cordeiro <pedro.henrique.cefetmg@gmail.com>
2018-12-19 16:39:32 -0200

    funcao que implementa sinal de inicio de jogo

--- sketch_dec13a.ino
+++ sketch_dec13a.ino
@@ -10,14 +10,13 @@
 
 #define NUMBER_OF_COLORS 3
 
+int number_of_colors = 1;
+
 void setup(){
     Serial.begin(9600);
     declarePorts();
-    int *colors = color_sorter();
-    
-    for(int a = 0 ; a < NUMBER_OF_COLORS ; a++){
-      flashes_led(colors[a]);
-    }
+    /* sinaliza o inicio do jogo */
+    begin_game();
 }
 
 void declarePorts()
@@ -38,23 +37,47 @@ void declarePorts()
 void loop(){
     Serial.begin(9600);
 
-    lights_up_from_the_push_button();
+    
 
 }
 
-int *color_sorter(){
+void begin_game(){
+  /* sinaliza inicio de jogo */
+  delay(2000);
+  float interval = 100;
+  while(interval >= 0 ){
+    digitalWrite(LED_GREEN , HIGH);
+    digitalWrite(LED_YELLOW , HIGH);
+    digitalWrite(LED_RED , HIGH);
+    digitalWrite(LED_BLUE , HIGH);
+    delay(interval);
+    digitalWrite(LED_GREEN , LOW);
+    digitalWrite(LED_YELLOW , LOW);
+    digitalWrite(LED_RED , LOW);
+    digitalWrite(LED_BLUE , LOW);
+    delay(interval);
+    interval = interval - 2 ;
+  }
+  digitalWrite(LED_GREEN , HIGH);
+  digitalWrite(LED_YELLOW , HIGH);
+  digitalWrite(LED_RED , HIGH);
+  digitalWrite(LED_BLUE , HIGH);
+  
+}
+
+int *color_sorter(int number_of_colors){
   /* lê a porta analogica 0 vazia*/ 
   /* para alimentar a funcao geradora de numeros aleatorios com um valor flutuante */
   randomSeed(analogRead(0));
   
   /* aloca 10 posicoes de memoria */
-  int *colors = malloc(NUMBER_OF_COLORS);
+  int *colors = malloc(number_of_colors);
   
   if(!colors){
     return NULL;
   }else{
     /* alocacao efetuada com sucesso */
-    for(int a = 0; a < NUMBER_OF_COLORS ; a++){
+    for(int a = 0; a < number_of_colors ; a++){
       /* popula as posicoes com cores aleatorias */
       colors[a] = random(LED_GREEN,LED_BLUE + 1);
     }
